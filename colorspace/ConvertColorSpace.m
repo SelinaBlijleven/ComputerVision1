f900b55e6b24a95ffe8f43e924bddb2522cd2867
thomasdegroot18 <thomas.degroot@student.fontys.nl>
2017-02-17 00:07:47 +0100

    assignment 2, grayscaling, and comments
    updated Comments of the second part of the assignment, and added in
    extra functionality of the grayscaling.

--- colorspace/ConvertColorSpace.m
+++ colorspace/ConvertColorSpace.m
@@ -18,17 +18,17 @@ function [new_image] = ConvertColorSpace(input_image, colorspace)
 input_image = im2double(input_image);
 
 if strcmp(colorspace, 'opponent')
-    new_image = rgb2opponent(input_image); % fill in this function
+    new_image = rgb2opponent(input_image);
 elseif strcmp(colorspace, 'rgb')  
-    new_image = rgb2normedrgb(input_image); % fill in this function
-elseif strcmp(colorspace, 'hsv')   
-    new_image = rgb2hsv(input_image);
+    new_image = rgb2normedrgb(input_image);
+elseif strcmp(colorspace, 'hsv')
     % use the built-in function
+    new_image = rgb2hsv(input_image);
 elseif strcmp(colorspace, 'ycbcr')
     % use the built-in function
     new_image = rgb2ycbcr(input_image);
 elseif strcmp(colorspace, 'gray')
-    new_image = rgb2grays(input_image); % fill in this function
+    new_image = rgb2grays(input_image);
 else
 % if user inputs an unknow colorspace just notify and do not plot anything
     fprintf('Error: Unknown colorspace type [%s]...\n',colorspace);
--- colorspace/visualize.m
+++ colorspace/visualize.m
@@ -1,8 +1,13 @@
 function visualize(input_image)
+%% The visualization of the images that have been transformed with the different functions.
 
+% Examining the image of locate if the image is a colored image or not:
+
+% Colored
 if size(input_image,3) == 3
     figure
     imshow(input_image)
+% Not Colored.
 elseif size(input_image,3) == 4
     figure
     for image_number = 1:4
@@ -10,5 +15,5 @@ elseif size(input_image,3) == 4
         imshow(input_image(:,:,image_number))
     end
 else
-    disp('Not correct')
+    disp('Not correct image loaded.')
 end
