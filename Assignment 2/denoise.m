faef8ffaa82ecf032020f6b7644c1974bec9ae2d
thomasdegroot18 <thomas.degroot@student.fontys.nl>
2017-02-24 17:37:30 +0100

    start of 2.2

--- Assignment 2/denoise.m
+++ Assignment 2/denoise.m
@@ -4,22 +4,32 @@ function imOut = denoise(image , kernel_type , kernel_size)
 % Image         =   The image that has to be denoised.
 % kernel_type   =   The type of kernel, "Box" or "Median"
 % kernel_size   =   The kernel size: 3x3, 5x5, or 7x7, (Box extra 9x9)
-
+A = [3 5 7 9];
+A = any(A(:) == kernel_size);
+B = [3 5 7];
+B = any(B(:) == kernel_size);
 
 % box filtering
-if (kernel_type == 'Box') && (any(kernel_size , [3,5,7,9]))
+if strcmp(kernel_type,'Box')  && A
     filter = ones(kernel_size,kernel_size);
     filter = filter*(1/(kernel_size^2));
     imOut = conv2(image,filter);
 
 % Median filtering
-elseif (kernel_type == 'Median') && (any(kernel_size , [3,5,7]))
-    filter = @(x) median(x(:));
-    imOut = nlfilter(image, [kernel_size, kernel_size], filter);
+elseif strcmp(kernel_type,'Median') && B
+    [xdim, ydim] = size(image);
+    imOut = zeros(xdim, ydim);
+    image = padarray(image,floor(kernel_size/2));
+    
+    for x=floor(kernel_size/2)+1:xdim-floor(kernel_size/2) %loop in x-dimension
+        for y=floor(kernel_size/2)+1:ydim-floor(kernel_size/2) % loop in y-dimension
+            neighbors = image(x-floor(kernel_size/2):x+floor(kernel_size/2),y-floor(kernel_size/2):y+floor(kernel_size/2));
+            imOut(x,y) = median(neighbors(:));
+        end
+    end
     
 else
     print('wrong kernel or kernel size');
     return
 end
-
 end
--- Assignment 2/myHistMatching.m
+++ Assignment 2/myHistMatching.m
@@ -3,4 +3,21 @@ function imOut = myHistMatching (input , reference )
 % takes input image and a reference image, and transforms the image in such
 % a way that is has the same histogram as the reference image.
 
+% Histogram image 1
+[pixelCounts_1, grayLevels_1] = imhist(input);
+
+%cumulative  histogram.
+cumsum_image_1 = cumsum(pixelCounts_1);
+bar(grayLevels_1, cumsum_image_1 , 'BarWidth', 1);
+
+
+% Histogram image 2
+[pixelCounts_2, grayLevels_2] = imhist(reference);
+
+%cumulative  histogram.
+cumsum_image_2 = cumsum(pixelCounts_2);
+bar(grayLevels_2, cumsum_image_2 , 'BarWidth', 1);
+
+
+
 end
