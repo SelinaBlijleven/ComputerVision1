bca82648165b775d1d15757f51ba9ca17c5ae1db
thomasdegroot18 <thomas.degroot@student.fontys.nl>
2017-03-13 12:46:07 +0100

    alignment

--- Assignment 4/Image_alignment.m
+++ Assignment 4/Image_alignment.m
@@ -10,16 +10,55 @@ P = 10;
 
 % Detect interest points in each image
 % Characterize the local appearance of the regions around interest points.
-[f_1,d_1] = vl_sift(image_1);
-[f_2,d_2] = vl_sift(image_2);
+% [f_1,d_1] = vl_sift(image_1);
+% [f_2,d_2] = vl_sift(image_2);
+% 
+% % Get the set of supposed matches between region descriptors in each image.
+% [matches, scores] = vl_ubcmatch(d_1, d_2);
 
-h1 = vl_plotframe(f_1(:,1:50)) ;
+f_1 = randi(679,1000,4)';
+f_2 = randi(679,1000,4)';
+matches = randi(1000,1000,2)';
+scores  = 3.*randn(1000,1)';
+
+perm = randperm(size(f_1,2)) ;
+sel = perm(1:50) ;
+imshow(image_1)
+hold on
+h1 = vl_plotframe(f_1(:,sel)) ;
+h2 = vl_plotframe(f_1(:,sel)) ;
 set(h1,'color','k','linewidth',3) ;
+set(h2,'color','y','linewidth',2) ;
+hold off
 
-% Get the set of supposed matches between region descriptors in each image.
-[matches, scores] = vl_ubcmatch(d_1, d_2);
+
+Match_coor = zeros(size(matches,1),4);
+
+f_1 = f_1';
+f_2 = f_2';
+matches = matches';
+scores = scores';
+for x=1:size(matches)
+    Match_coor(x,:) = [f_1(matches(x,1),1:2),f_2(matches(x,2),1:2)];
+end
+
 
 % Perform RANSAC to discover the best transformation between images.
+transformation_matrix = ransac(N, P, Match_coor);
 
+out_image = zeros(size(image_1));
+for x=1:size(out_image,1)
+    for y=1:size(out_image,2)
+        A = [x y 0 0 1 0; 0 0 x y 0 1];
+        coor_new = A*transformation_matrix;
+        x_new = min(max(1,round(coor_new(1))),size(out_image,1));
+        y_new = min(max(1,round(coor_new(2))),size(out_image,2));
+        out_image(x_new,y_new) = image_2(x,y);
+    end
+end
+
+
+
+% imshow(out_image)
 
 end
--- Assignment 4/ransac.m
+++ Assignment 4/ransac.m
@@ -13,7 +13,6 @@ function transformation = ransac(N, P, T)
     
     % Size of T
     [tm, tn] = size(T);
-
     % Repeat N times
     for i = 1:N
         
@@ -25,24 +24,25 @@ function transformation = ransac(N, P, T)
 
         % Construct a matrix A and vector b using the P pairs of points and 
         % find transformation parameters
-        A = sample(:, :tn-1);
-        b = sample(:, tn-1);
+        elem_coor = sample(1, 1:2);
+        A = [elem_coor(1) elem_coor(2) 0 0 1 0; 0 0 elem_coor(1) elem_coor(2) 0 1];
+        b = sample(1, 3:4)';
         x = pinv(A) * b;
-
+        
         % Transform all locations of T points
-        T_A = T(:, :tn-1);
-        T_b = T(:, tn-1);
-        estimate_b = T_A * x;
-
         % Count the number of inliers, inliers being de?ned as the number of 
         % transformed points from image1 that lie within a radius of 10 pixels 
         % of their pair in image2. 
-        for j = 1:size(T_b)
-            if abs(T_b(j) - estimate_b(j))
+        for j = 1:size(T)
+            elem_coor_T = T(j, 1:2);
+            T_A = [elem_coor_T(1) elem_coor_T(2) 0 0 1 0; 0 0 elem_coor_T(1) elem_coor_T(2) 0 1];
+            T_b = T(j, 3:4)';
+            estimate_b = T_A * x;
+            if abs(T_b - estimate_b)
                 sample_inliers = [sample_inliers;T(i, :)];
             end
         end
-        sample_total = size(sample_inliers)
+        sample_total = size(sample_inliers);
 
         % If this count exceeds the best total so far, save the transformation parameters and the set of inliers.
         if sample_total > best_total
@@ -57,5 +57,5 @@ function transformation = ransac(N, P, T)
     % in the scene should correspond to its pose in image2. To transform 
     % the image, implement your own function based on nearest-neighbor interpolation. 
     % Then use the built-in MATLAB functions imtransform and maketform and compare your results.
-    transformation = []
+    transformation = best_x
 end
