bc5ae318e2f90d767ae6236fa64c9efb8b04209a
thomasdegroot18 <thomasdegroot18@github.com>
2017-03-23 14:43:26 +0100

    K_means and updated histograms

--- FinalProject/main_part_1.m
+++ FinalProject/main_part_1.m
@@ -15,31 +15,34 @@ imd_train = imageDatastore(fullfile(path_to_loc, categories_1), 'LabelSource', '
 imd_test = imageDatastore(fullfile(path_to_loc, categories_2), 'LabelSource', 'foldernames');
 
 % Step 1: Feature extraction.
-SIFT_matrix = ones(4,1);
+SIFT_matrix = double(ones(128,1));
 
 for i = 1:10
     imd_train.Labels(i); % Gives the label of the image that is read
     img = readimage(imd_train,i);
-    
+    % Extract the SIFT features to create a K_means matrix needed to find
+    % the centres fot the vocal words.
     [f, d] = feature_extraction(img, 'SIFT');
-    SIFT_matrix = [SIFT_matrix, f];   
+    SIFT_matrix = [SIFT_matrix, double(d)];   
 end
 
 % Step 2: Visual vocabulary
 [C] = vl_kmeans(SIFT_matrix,400,'MaxNumIterations',100);
-
-C
+% Transpose to get correct columns
+C = C';
 
 % Step 3: Quantize features using vocabulary
-
-
-
-
-% Step 4: Representing images by frequencies
-
-
-
+for i = 20:30
+    imd_train.Labels(i); % Gives the label of the image that is read
+    img = readimage(imd_train,i);
+    [f, d] = feature_extraction(img, 'SIFT');
+    k = dsearchn(C,double(d)');
+    % Step 4: Representing images by frequencies
+    h = histogram(k,400);
+end
 
 % Step 5: Classification.
+
+
 
 end 
