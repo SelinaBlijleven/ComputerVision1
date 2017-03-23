8baa4eea2194367a1e83951ac004d7f9283a2ba0
thomasdegroot18 <thomasdegroot18@github.com>
2017-03-23 14:11:36 +0100

    main_kmeans

--- FinalProject/main_part_1.m
+++ FinalProject/main_part_1.m
@@ -15,21 +15,20 @@ imd_train = imageDatastore(fullfile(path_to_loc, categories_1), 'LabelSource', '
 imd_test = imageDatastore(fullfile(path_to_loc, categories_2), 'LabelSource', 'foldernames');
 
 % Step 1: Feature extraction.
-n = 25;
-SIFT_matrix = ones(1, n);
+SIFT_matrix = ones(4,1);
 
 for i = 1:10
     imd_train.Labels(i); % Gives the label of the image that is read
     img = readimage(imd_train,i);
-    SIFT_matrix = [SIFT_matrix; ones(1, n)]; % sift(img,'sift')];   
+    
+    [f, d] = feature_extraction(img, 'SIFT');
+    SIFT_matrix = [SIFT_matrix, f];   
 end
 
 % Step 2: Visual vocabulary
+[C] = vl_kmeans(SIFT_matrix,400,'MaxNumIterations',100);
 
-[LABELS, A, CLUST] = kmeansj(SIFT_matrix,400,50);
-
-
-
+C
 
 % Step 3: Quantize features using vocabulary
 
